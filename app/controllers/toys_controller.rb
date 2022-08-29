168c870302b57d06d823436285daf23395780450
Joshua Welsh <j22yosh@gmail.com>
2022-08-28 23:59:06 -0400

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,7 +7,7 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
@@ -22,10 +22,19 @@ class ToysController < ApplicationController
     head :no_content
   end
 
+  def increment_likes
+    toy = Toy.find(params[:id])
+    if toy
+      toy.update(toy_params: toy.likes + 1)
+      render json: toy
+    else
+      render json: {error: "Toy not found"}, status: :not_found
+    end
+  end
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:id, :name, :image, :likes)
   end
 
 end
